7c9cbdf909a504f319f6d4932811248b033d4fdc
LeonardoJohnAndrew <147904373+leonardo-JohnAndrew@users.noreply.github.com>
2026-06-15 08:21:25 +0800

    Add files via upload

---
+++ nstrendb (2).sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 15, 2026 at 02:20 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nstrendb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `budgetitems`
+--
+-- Error reading structure for table nstrendb.budgetitems: #1932 - Table &#039;nstrendb.budgetitems&#039; doesn&#039;t exist in engine
+-- Error reading data for table nstrendb.budgetitems: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `nstrendb`.`budgetitems`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `budgetvalues`
+--
+-- Error reading structure for table nstrendb.budgetvalues: #1932 - Table &#039;nstrendb.budgetvalues&#039; doesn&#039;t exist in engine
+-- Error reading data for table nstrendb.budgetvalues: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `nstrendb`.`budgetvalues`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cashbooks`
+--
+-- Error reading structure for table nstrendb.cashbooks: #1932 - Table &#039;nstrendb.cashbooks&#039; doesn&#039;t exist in engine
+-- Error reading data for table nstrendb.cashbooks: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `nstrendb`.`cashbooks`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `checkitems`
+--
+-- Error reading structure for table nstrendb.checkitems: #1932 - Table &#039;nstrendb.checkitems&#039; doesn&#039;t exist in engine
+-- Error reading data for table nstrendb.checkitems: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `nstrendb`.`checkitems`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `checks`
+--
+-- Error reading structure for table nstrendb.checks: #1932 - Table &#039;nstrendb.checks&#039; doesn&#039;t exist in engine
+-- Error reading data for table nstrendb.checks: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `nstrendb`.`checks`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+-- Error reading structure for table nstrendb.departments: #1932 - Table &#039;nstrendb.departments&#039; doesn&#039;t exist in engine
+-- Error reading data for table nstrendb.departments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `nstrendb`.`departments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `itemslists`
+--
+-- Error reading structure for table nstrendb.itemslists: #1932 - Table &#039;nstrendb.itemslists&#039; doesn&#039;t exist in engine
+-- Error reading data for table nstrendb.itemslists: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `nstrendb`.`itemslists`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+-- Error reading structure for table nstrendb.notifications: #1932 - Table &#039;nstrendb.notifications&#039; doesn&#039;t exist in engine
+-- Error reading data for table nstrendb.notifications: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `nstrendb`.`notifications`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ph_cash_banks`
+--
+-- Error reading structure for table nstrendb.ph_cash_banks: #1932 - Table &#039;nstrendb.ph_cash_banks&#039; doesn&#039;t exist in engine
+-- Error reading data for table nstrendb.ph_cash_banks: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `nstrendb`.`ph_cash_banks`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchaseitems`
+--
+-- Error reading structure for table nstrendb.purchaseitems: #1932 - Table &#039;nstrendb.purchaseitems&#039; doesn&#039;t exist in engine
+-- Error reading data for table nstrendb.purchaseitems: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `nstrendb`.`purchaseitems`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchases`
+--
+-- Error reading structure for table nstrendb.purchases: #1932 - Table &#039;nstrendb.purchases&#039; doesn&#039;t exist in engine
+-- Error reading data for table nstrendb.purchases: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `nstrendb`.`purchases`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers`
+--
+-- Error reading structure for table nstrendb.suppliers: #1932 - Table &#039;nstrendb.suppliers&#039; doesn&#039;t exist in engine
+-- Error reading data for table nstrendb.suppliers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `nstrendb`.`suppliers`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table nstrendb.users: #1932 - Table &#039;nstrendb.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table nstrendb.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `nstrendb`.`users`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `us_cash_banks`
+--
+-- Error reading structure for table nstrendb.us_cash_banks: #1932 - Table &#039;nstrendb.us_cash_banks&#039; doesn&#039;t exist in engine
+-- Error reading data for table nstrendb.us_cash_banks: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `nstrendb`.`us_cash_banks`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
